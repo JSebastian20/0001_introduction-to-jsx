a90cb94dc7f4e1a281386ee539fb8943d16a2bf1
JSebastian20 <62495587+JSebastian20@users.noreply.github.com>
2020-03-22 22:51:47 +1030

    added function
    added function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "James";
+const lName = "Sebastian";
+const num = 7;
+
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
+    <h1>
+      Hello {fName} {lName}
+    </h1>
     <p>this is a paragraph</p>
+    <p>The number is {num}</p>
+    <p>The second number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
